016e1f89aeeafb11f9dae6c26d0ef6fb90980d0e
Josh Andrews <jangles1602@gmail.com>
2017-11-03 08:34:50 -0400

    All tests complete

--- Milestone 1/VGA_DEMO/Controller.v
+++ Milestone 1/VGA_DEMO/Controller.v
@@ -7,14 +7,12 @@ module Controller(
 	output reg mem_reg_dst);
 	
 	always @* begin
-		if (ins[5:0] == 6'b100000) begin
+		if (ins[31:26] == 6'b000000) begin
 			reg_write = 1;
 			mem_reg_dst = 0;
 			reg_dest = 0;
 		end else begin
 			reg_write = 0;
-			mem_reg_dst = 0;
-			reg_dest = 0;
 		end
 	end
 endmodule
